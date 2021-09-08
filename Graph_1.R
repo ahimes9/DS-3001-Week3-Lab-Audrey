24bba19cdb9955fe422d7c6c5e04fd90cebf28fa
Audrey Himes <audreyhimes@d-172-25-112-105.dhcp.virginia.edu>
2021-09-08 15:59:43 -0400

    step 3 on audrey (audrey)

--- Graph_1.R
+++ Graph_1.R
@@ -1,16 +1,20 @@
-# AUdrey's file
+## Audrey's file
 
 library(tidyverse)
 # load packages
 
-# STEP 1: Hayden
+## STEP 1: Hayden
 arrests <- read_csv("USArrests.csv")
 
 # load in dataset
 
-# STEP 2: Madeleine
+## STEP 2: Madeleine
 library(ggplot2)
+
+# creating a bar graph
 plot <- ggplot(arrests, aes(x=Assault)) + geom_histogram(bins = 10)
+plot
 
-# STEP 3: Audrey
-
+## STEP 3: Audrey
+finished_plot <- plot + ggtitle("# of Locations with equal # of Assaults") + xlab("# of Assaults") + ylab("# of places")
+finished_plot
